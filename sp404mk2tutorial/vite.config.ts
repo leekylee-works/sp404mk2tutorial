873785903399d0dcc3d931d0db72d7878b86fbd0
leekylee-works <rizemonkey@gmail.com>
2025-12-12 14:55:04 +0800

    First upload complete SP-404MKII interactive tutorial

---
+++ sp404mk2tutorial/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
